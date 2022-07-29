363a9bdbb26889ce52d5f60528e6894e7ca41f43
Carlos Buendia <cbuendia1984@gmail.com>
2022-07-28 21:30:42 -0500

    Se añaden todos los modelos a usar

--- Proyecto_Entrega/App_Entrega/models.py
+++ Proyecto_Entrega/App_Entrega/models.py
@@ -1,3 +1,22 @@
+from mailbox import NoSuchMailboxError
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=80)
+    apellido =models.CharField(max_length=80)
+    correo =models.EmailField()
+    actividad =models.CharField(max_length=80)
+    fecha = models.DateField()
+
+class Colaborador(models.Model):
+    nombre = models.CharField(max_length=80)
+    apellido =models.CharField(max_length=80)
+    telefono = models.IntegerField()
+    correo =models.EmailField()
+    Negocio = models.CharField(max_length=80)  
+
+class Buzon(models.Model):
+    nombre = models.CharField(max_length=80)
+    apellido =models.CharField(max_length=80)
+    correo =models.EmailField()
+    sugerencia =models.CharField(max_length=80)
